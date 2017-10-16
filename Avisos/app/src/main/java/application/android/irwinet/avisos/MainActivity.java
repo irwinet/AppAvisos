0cfe8362852ca32945fd2371ed8b9e06270ce828
irwinet <32725479+irwinet@users.noreply.github.com>
2017-10-15 23:29:23 -0500

    add icon

--- Avisos/app/src/main/java/application/android/irwinet/avisos/MainActivity.java
+++ Avisos/app/src/main/java/application/android/irwinet/avisos/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -39,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
         lvAvisos = (ListView) findViewById(R.id.lvAvisos);
         findViewById(R.id.lvAvisos);
         lvAvisos.setDivider(null);
